24c3d81c066446fb18639e003bfb60f1a468390d
Corey Jackson <cjacks04@syr.edu>
2022-07-23 11:35:40 -0500

    dic. export

--- community/vocab_extract.R
+++ community/vocab_extract.R
@@ -3,11 +3,14 @@ library(readr)
 library(tm)
 library(dplyr)
 library(stringi)
-malletwords <- scan("~/Box/Research/Language Studies (CS)/Analysis/Data/mallet.txt", character(), quote = "")
+library(tidytext)
+library(textstem)
+
+malletwords <- scan("/Volumes/cbjackson2/language models/mallet.txt", character(), quote = "")
 
 
 #######  import comments from Gravity Spy
-gravityspy_comments <- read_csv("~/Box/Research/Language Studies (CS)/Analysis/Data/gravity-spy-comments.csv")
+gravityspy_comments <- read_csv("/Volumes/cbjackson2/language models/projects/gravityspy/gravity-spy-comments.csv")
 
 # Data transformations
 gravityspy_comments$comment_body <-  gsub("http[[:alnum:][:punct:]]*", "", gravityspy_comments$comment_body)# remove URLs
@@ -27,10 +30,10 @@ gravityspy_unigram <- gravityspy_comments %>% unnest_tokens(unigram,
 gravityspy_unigram$unigram  <- lemmatize_words(gravityspy_unigram$unigram)
 
 gravitspy_words <- unique(gravityspy_unigram$unigram)
-remove(gravityspy_unigram,gravityspy_comments)
+#remove(gravityspy_unigram,gravityspy_comments)
 
 ####### import comments from Snapshot
-snapshot_comments <- read_csv("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/snapshot_prep2.csv")
+snapshot_comments <- read_csv("/Volumes/cbjackson2/language models/projects/snapshot/snapshot_prep2.csv")
 
 snapshot_comments$comment <-  gsub("http[[:alnum:][:punct:]]*", "", snapshot_comments$comment)# remove URLs
 snapshot_comments$comment <-  gsub("http[^[:space:]]*", "", snapshot_comments$comment) # remove non-ASCII characters
@@ -49,10 +52,10 @@ snapshot_unigram <- snapshot_comments %>% unnest_tokens(unigram,
 snapshot_unigram$unigram  <- lemmatize_words(snapshot_unigram$unigram)
 
 snapshot_words <- unique(snapshot_unigram$unigram)
-remove(snapshot_unigram,snapshot_comments)
+#remove(snapshot_unigram,snapshot_comments)
 
 #######  import comments from Planet Hunters
-planet_comments <- read_csv("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/PHComments.csv")
+planet_comments <- read_csv("/Volumes/cbjackson2/language models/projects/planethunters/planethunters.csv")
 planet_comments <- as.data.frame(planet_comments)
 names(planet_comments)[5] <- "comment"
 
@@ -74,21 +77,23 @@ planet_unigram <- planet_comments %>% unnest_tokens(unigram,
 planet_unigram$unigram  <- lemmatize_words(planet_unigram$unigram)
 
 planet_words <- unique(planet_unigram$unigram)
-remove(planet_unigram,planet_comments)
+#remove(planet_unigram,planet_comments)
 
 # domain science words (get those in Snapshot)
 citizen_science_words <- gravitspy_words[gravitspy_words %in% snapshot_words] 
-setwd("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/")
-write.table(citizen_science_words,"citizen_science_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
+citizen_science_words <- citizen_science_words[citizen_science_words %in% planet_words] 
+
+#setwd("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/")
+write.table(citizen_science_words,"/Volumes/cbjackson2/language models/citizen_science_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
 
 # astonomy science words (get those also in Planet Hunters)
-astronomy_words <- gravitspy_words[gravitspy_words %in% planet_words & ! gravitspy_words %in% citizen_science_words] 
-setwd("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/")
-write.table(astronomy_words,"astronomy_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
+astronomy_words <- gravitspy_words[gravitspy_words %in% planet_words] 
+astronomy_words <- astronomy_words[!astronomy_words %in% citizen_science_words] 
+write.table(astronomy_words,"/Volumes/cbjackson2/language models/astronomy_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
 
 # gravity spy domain science words (get those not in Planet Hunters and not in Snapshot)
-domain_science_words <- gravitspy_words[!gravitspy_words %in% astronomy_words & !gravitspy_words %in% citizen_science_words] 
-setwd("~/Box/Research/Language Studies (CS)/Analysis/Extracting Vocabulary/")
-write.table(domain_science_words,"domain_science_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
+domain_science_words <- gravitspy_words[!gravitspy_words %in% astronomy_words] 
+domain_science_words <- domain_science_words[!domain_science_words %in% citizen_science_words] 
+write.table(domain_science_words,"/Volumes/cbjackson2/language models/domain_science_words.txt", row.names=FALSE,sep="\t", quote = FALSE)
 
 
